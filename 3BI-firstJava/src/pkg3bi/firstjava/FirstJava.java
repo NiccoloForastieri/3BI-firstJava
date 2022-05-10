33a9b89c75798cbd7cf9bb20dbfb5450c65f63b3
Forastieri Niccolo <forastieri.niccolo@osdbsesto>
2022-05-10 13:35:27 +0200

    remove push

--- 3BI-firstJava/src/pkg3bi/firstjava/FirstJava.java
+++ 3BI-firstJava/src/pkg3bi/firstjava/FirstJava.java
@@ -15,7 +15,9 @@ public class FirstJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        for(int i = 0;;i++){
+            System.out.println(i);
+        }
     }
     
 }
